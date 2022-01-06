022cd7c7cd84fe403984bdf433f874dc9f6a343a
MinsterRobin <minster.robin.pro@gmail.com>
2022-01-06 18:32:01 +0100

    Fix imports globals des variables

---
+++ src/views/ViewTodoList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="test">lalalalal</div>
+</template>
+
+<script>
+    export default {
+        name: "ViewTodoList"
+    }
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+    @import "src/scss/main";
+
+    .test {
+        font: {
+            family: $font-family-secondary;
+            size: $font-size-l;
+            weight: 600;
+        }
+    }
+</style>
